90ffe067e01674077c71d3f406850c377e643126
Kayle7777 <jessewebb2@gmail.com>
2018-07-18 20:48:40 -0500

    stuff

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,8 @@ function popitup(url,windowName) {
        if (window.focus) {newwindow.focus()}
        return false;
 }
-let uriRedirectTest = "https://127.0.0.1/apiredirect.html"
-spotifyWindow = `https://accounts.spotify.com/authorize?client_id=0bfbe170f82c46a089b7d9d412592492&redirect_uri=${uriRedirectTest}/&response_type=code`
+let uriRedirectTest = "http://127.0.0.1/apiredirect.html"
+spotifyWindow = `http://accounts.spotify.com/authorize?client_id=0bfbe170f82c46a089b7d9d412592492&redirect_uri=${uriRedirectTest}/&response_type=code`
 
 popitup(spotifyWindow, "test")
 //
