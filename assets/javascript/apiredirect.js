fbc88e8e4c7d97ad35da01585c056598c1b2a07c
Kayle7777 <jessewebb2@gmail.com>
2018-07-19 12:50:29 -0500

    User clicks a button to begin auth process, instead of popup automatically coming

--- assets/javascript/apiredirect.js
+++ assets/javascript/apiredirect.js
@@ -3,4 +3,4 @@ var newURL = new URL(window.location.href);
 var code = newURL.searchParams.get("code")
 
 localStorage.setItem("spotifyAPIcode", code)
-// window.close();
+window.close();
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,12 +2,20 @@
 function popitup(url,windowName) {
        newwindow=window.open(url,windowName,'height=700,width=400');
        if (window.focus) {newwindow.focus()}
-       return false;
 }
 let uriRedirectTest = "http://127.0.0.1:8080/apiredirect.html"
 spotifyWindow = `http://accounts.spotify.com/authorize?client_id=0bfbe170f82c46a089b7d9d412592492&redirect_uri=${uriRedirectTest}&response_type=code`
 
-popitup(spotifyWindow, "test")
+
+if (localStorage.getItem("spotifyAPIcode") === null) {
+  let linkButton = $("<button class='btn btn-warning'>Log in to Spotify</button>");
+  linkButton.click(() => {
+    popitup(spotifyWindow, "spotifyAPIAuth");
+    $("#apiWarningModal").modal('hide');
+  });
+  $("#spotifyLinker").append(linkButton);
+  $("#apiWarningModal").modal();
+}
 //
 // $.ajax(
 //   {
