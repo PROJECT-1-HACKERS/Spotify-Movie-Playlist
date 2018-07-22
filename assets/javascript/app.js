69a63e38f8eb58e6bdd67b13e17a1066a59890bf
Kayle7777 <jessewebb2@gmail.com>
2018-07-22 09:55:10 -0500

    This works for me, I hope it doesn't break your code. Please do npm install -g live-server and run 'live-server' in this page, to test the spotify stuff. open console

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,12 +30,26 @@ function imdbAjax(name, callback){
       // Target specific div needed
       // var $body = $("#soundtracks_content", body).children().children();
 
-      var $body = $("#soundtracks_content", body).children().children();
-      console.log($body)
-
-      for (var i = 0; i < $body.length; i++) {
-        // logs song title
-        callback({"trackName": Array.from($($body[i]).contents())[0].data, "artist": Array.from($($body[i]).contents().closest('a:first-of-type').text()).join("")});
+      // This checks a specific spot in some of these soundtrack pages, for a huge piece of json that has all the data we need, in a specific react script tag Some have it, some don't.
+      // This will increase our accuracy, webscraping is hard because we are at the whims of the host-site gods, and however they decided to format that particular page
+      // =========================================================================================================
+      if ($(body).find('script:contains("trackIds")').text().includes(',')) {
+        let scriptJson = $(body).find('script:contains("trackIds")').text();
+        scriptJson = scriptJson.replace(/.+?(?=\{"albumOpenS)/, '["center-1-react",');
+        scriptJson = scriptJson.replace(/Samples\"\}\].*/, 'Samples"}]');
+        let jsonObj = JSON.parse(scriptJson);
+        for (key in jsonObj[1].albumData.tracks) {
+          callback({"isTrue": true, "trackName": jsonObj[1].albumData.tracks[key].trackTitle, "artist": jsonObj[1].albumData.tracks[key].artists});
+        }
+      }
+      // Otherwise, do it how we used to do it
+      // =========================================================================================================
+      else{
+        var $body = $("#soundtracks_content", body).children().children();
+        for (var i = 0; i < $body.length; i++) {
+          // logs song title
+          callback({"isTrue": false, "trackName": Array.from($($body[i]).contents())[0].data, "artist": Array.from($($body[i]).contents().closest('a:first-of-type').text()).join("")});
+        }
       }
     })
   })
--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,11 +1,38 @@
+let userID;
+
 function movieInputClick(callback) {
   let moVal = $("#movieInput").val().trim();
   let movie = callback(new Movie(moVal));
 }
 
-
+let spotifyTrackIdList = [];
 $("#movieInputSubmit").click(function() {
-  movieInputClick((x) => {
-      console.log(x)
-  })
+  spotifyTrackIdList = [];
+  getUserId((x) => userID = x);
+  movieInputClick(
+    (data) => {
+      setTimeout(function() {
+        $("#soundTrackList").empty();
+        let trackNames = data.trackInfo.map((x) => x.trackName);
+        console.log(trackNames)
+        trackNames.map((eachName) => $("#soundTrackList").append(`<li class='list-group-item'>${eachName}</li>`));
+        songIdGet(trackNames, (ajaxResponse) => {
+          for (var i = 0; i < ajaxResponse.length; i++) {
+            // console.log(ajaxResponse[i].tracks.items[0].id)
+            spotifyTrackIdList.push(ajaxResponse[i].tracks.items[0].id)
+          }
+        });
+      }, 900)
+    })
 })
+
+
+
+
+
+// function() {
+//   let songList = x.trackInfo.map((y) => y.trackName)
+//   songIdGet(songList, (x) => {
+//     spotifyTrackIdList = x.map((y) => {return y.tracks.items[0].id})
+//     console.log(spotifyTrackIdList)
+//   }
