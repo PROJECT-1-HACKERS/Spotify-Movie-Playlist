447b7ba566afded11c8a8b096b5f6f47f8a55826
Kayle7777 <jessewebb2@gmail.com>
2018-07-23 12:40:19 -0500

    Awesome fix here -- can delete a lot of stuff from my spotifyAPI.js page. This basically changes the architecture of how it searches for songs. Originally, it was tied to the movie search itself, and would try to build an array of spotify IDs at the same time that it populates the page with movie soundtrack names. Now, it does it all 1 at a time, seems to work MUCH better.

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -13,49 +13,55 @@ $("#movieInputSubmit").click(function() {
   movieInputClick(
     (data) => {
       setTimeout(function() {
+        let playlistId;
         $("#soundTrackList").empty();
         let trackNames = data.trackInfo.map((x) => x.trackName);
-        // console.log(trackNames)
         trackNames.map((eachName, index) => $("#soundTrackList").append(`<li class='list-group-item'>${index+1}: ${eachName}</li>`));
-        songIdGet(trackNames, (ajaxResponse) => {
-          for (var i = 0; i < ajaxResponse.length; i++) {
-            // console.log(ajaxResponse[i].tracks.items[0].id)
-            spotifyTrackIdList.push(ajaxResponse[i].tracks.items[0].id)
-          }
-        });
-        let playlistButton = $("<button>");
-        playlistButton.text("Create Spotify Playist");
-        playlistButton.attr("class", "btn btn-outline-success");
+// ====================================================================================
+// Create one new playlist
+        $.ajax({
+          method: "POST",
+          url: `https://api.spotify.com/v1/users/${userId}/playlists`,
+          headers: {
+            "Authorization": "Bearer "  + localStorage.getItem('spotifyAPItoken'),
+            "Content-Type": "application/json"
+          },
+          data: JSON.stringify({name: `${movieVal} soundtrack playlist`})
+        }).then(function(newPlaylist) {
+          playlistId = newPlaylist.id
+        })
+// ====================================================================================
+// Map over trackNames array, search and grab the first song, then add it to playlist.
+        let playlistButton = $("<button class='btn btn-warning'>");
+        playlistButton.text("Create Spotify Playlist");
         playlistButton.click(function() {
-          console.log(spotifyTrackIdList)
-          $.ajax({
-            method: "POST",
-            url: `https://api.spotify.com/v1/users/${userId}/playlists`,
-            headers: {
-              "Authorization": "Bearer "  + localStorage.getItem('spotifyAPItoken'),
-              "Content-Type": "application/json"
-            },
-            data: JSON.stringify({name: `${movieVal} soundtrack playlist`})
-          }).then(function(newPlaylist) {
-            let playlistId = newPlaylist.id;
-            let dumbUrl = `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks?uris=`
-            for (var i = 0; i < spotifyTrackIdList.length; i++) {
-              if(i == spotifyTrackIdList.length-1) {
-                dumbUrl = dumbUrl+ "spotify%3Atrack%3A"+spotifyTrackIdList[i]
-
-              }else {
-                dumbUrl = dumbUrl+ "spotify%3Atrack%3A"+spotifyTrackIdList[i]+","
-              }
-            }
+          console.log(trackNames);
+          for (var i = 0; i < trackNames.length; i++) {
             $.ajax({
-              method: "POST",
-              url: dumbUrl,
+              method: "GET",
+              url: "https://api.spotify.com/v1/search",
               headers: {
-                "Authorization": "Bearer " + localStorage.getItem('spotifyAPItoken'),
-                "Content-Type": "application/json"
-              }
+                "Authorization": 'Bearer ' + localStorage.getItem('spotifyAPItoken')
+              },
+              data: {
+                "q": trackNames[i],
+                "type": "track",
+                "limit": "1"
+              },
+            }).then(function(response) {
+              let trackId = response.tracks.items[0].id;
+              let dumbUrl = `https://api.spotify.com/v1/users/${userId}/playlists/${playlistId}/tracks?uris=`
+              dumbUrl = dumbUrl+ "spotify%3Atrack%3A"+trackId;
+              $.ajax({
+                method: "POST",
+                url: dumbUrl,
+                headers: {
+                  "Authorization": "Bearer " + localStorage.getItem('spotifyAPItoken'),
+                  "Content-Type": "application/json"
+                }
+              })
             })
-          })
+          }
         })
         $("#createPlaylistButton").append(playlistButton);
       }, 900)
