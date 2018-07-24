c8d4813d1aa325cf14740e184a5b190a56f8468b
V'Ana <vanagavriela@gmail.com>
2018-07-24 11:26:33 -0500

    added hero mp4 in bckgrnd

--- assets/javascript/apiredirect.js
+++ assets/javascript/apiredirect.js
@@ -1,7 +1,6 @@
 var newURL = new URL('http://www.google.com/?'+window.location.hash.substr(1));
 
 var token = newURL.searchParams.get("access_token");
-token = "BQDpqNjjLiIhJ9I8Rk_M_jC9vtyZH5B_qrvCssp03c87e-9a-vcx0SAJZ_s2ivIrC0ZH6DKlRB_M4tcJoEK-6g8RD3WAhqh74rYMp8oKV6v-XtTZ0NQ27Tsvdc7ank-KeO6hhxzC-7X1h5awC9B2uCUbFbwfagRGKPCX5YWvoIOu9FCGNslD3JpDjePVeAt9YuUdrplAUf42Yp8f8u_T7ymhfLoNAw";
 var tokenExpiry = newURL.searchParams.get("expires_in");
 
 localStorage.setItem("spotifyAPItoken", token);
