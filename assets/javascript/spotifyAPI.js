f068ab309a31f1813bb45e7150d66bca9b3c42aa
Kayle7777 <jessewebb2@gmail.com>
2018-07-28 10:01:24 -0500

    We done

--- assets/javascript/spotifyAPI.js
+++ assets/javascript/spotifyAPI.js
@@ -10,7 +10,7 @@ function popitup(url,windowName) {
        newwindow=window.open(url,windowName,'height=700,width=400');
        if (window.focus) {newwindow.focus()}
 }
-let uri_Redirect = ["http://127.0.0.1:8080/apiredirect.html"]
+let uri_Redirect = ["https://project-1-hackers.github.io/Project-1-BootCamp/apiredirect.html"]
 let scopes = "user-read-email user-read-private user-read-birthdate playlist-modify-public playlist-modify-private"
 spotifyWindow = `http://accounts.spotify.com/authorize?client_id=0bfbe170f82c46a089b7d9d412592492&redirect_uri=${uri_Redirect[0]}&scope=${scopes}&response_type=token`
 
